d1b122f0f97d3fe7776c33e692a0e783e315668c
shinnosuketouma <touma.fromm@gmail.com>
2026-03-10 12:55:51 +0900

    Create FfSpeechQueriesPlugin.kt

---
+++ android/src/main/kotlin/com/example/ff_speech_queries/FfSpeechQueriesPlugin.kt
@@ -0,0 +1,7 @@
+package com.example.ff_speech_queries
+import io.flutter.embedding.engine.plugins.FlutterPlugin
+
+class FfSpeechQueriesPlugin: FlutterPlugin {
+    override fun onAttachedToEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
+    override fun onDetachedFromEngine(binding: FlutterPlugin.FlutterPluginBinding) {}
+}
